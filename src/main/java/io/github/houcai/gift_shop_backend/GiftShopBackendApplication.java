2e7814f25e4c138922190ae1e88b8c47d602911b
houcai <lishannng@hotmail.com>
2026-01-01 19:33:07 +0100

    create empty project

---
+++ src/main/java/io/github/houcai/gift_shop_backend/GiftShopBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.houcai.gift_shop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/houcai/gift_shop_backend/GiftShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.houcai.gift_shop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
