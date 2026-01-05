2ee08e15cb451cd730721f9271d69c38d2ded8bd
houcai <lishannng@hotmail.com>
2026-01-05 20:11:33 +0100

    Feature: Spring cloud config server.

---
+++ config-server/src/main/java/io/github/houcai/gift_shop_backend_config_server/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package io.github.houcai.gift_shop_backend_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
