ecea70af924b55ad92d7ea7ef32c9b6ff90ebfc9
houcai <lishannng@hotmail.com>
2026-01-06 22:04:33 +0100

    Feature: Add a gateway filter showing every incoming request.

---
+++ gateway/src/main/java/io/github/houcai/gift_shop_backend_gateway/LoggingFilter.java
@@ -0,0 +1,22 @@
+package io.github.houcai.gift_shop_backend_gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Incoming request to: {}", exchange.getRequest().getPath());
+        // Pass the request to next global filter -> downstream service.
+        return chain.filter(exchange);
+    }
+}
