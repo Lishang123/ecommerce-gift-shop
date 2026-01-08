c7b7c4cf53fee105469a756b074621b0665f64ba
houcai <lishannng@hotmail.com>
2026-01-08 21:55:47 +0100

    Feature: setting up rate limiter for gateway.

--- gateway/src/main/java/io/github/houcai/gift_shop_backend_gateway/GatewayConfig.java
+++ gateway/src/main/java/io/github/houcai/gift_shop_backend_gateway/GatewayConfig.java
@@ -1,12 +1,13 @@
 package io.github.houcai.gift_shop_backend_gateway;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -14,25 +15,53 @@ import java.time.Duration;
 public class GatewayConfig {
 
     @Bean
+    public RedisRateLimiter redisRateLimiter(){
+        // Allow 10 tokens per second on average.
+        // can suddenly handle up to 20 tokens at once.
+        // each request consumes 1 token.
+        return new RedisRateLimiter(10, 20, 1);
+    }
+
+    @Bean
+    public KeyResolver hostNameKeyResolver(){
+        return exchange ->
+                Mono.justOrEmpty(exchange.getRequest().getRemoteAddress())
+                        .map(addr -> addr.getHostName())
+                        .switchIfEmpty(Mono.just("unknown"));
+    }
+
+    @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
+                /*
+                    Rate limit first, then circuit breaker, then retry:
+                    RateLimiter rejects excess traffic immediately (cheap).
+                    CircuitBreaker blocks calls when downstream is unhealthy (also cheap).
+                    Retry only happens when the call is actually attempted, and you avoid “retrying” 429s / OPEN-breaker fast-fails.
+                 */
                 .route("user-service",
                         r -> r.path("/api/users/**")
                                 //.filters(f -> f.rewritePath("/users(?<segment>/?.*)",
                                 //        "/api/users${segment}"))
                                 .filters(f ->
-                                        f.retry(config -> config
-                                                .setRetries(10)
-                                                .setMethods(HttpMethod.GET))
+                                        f.requestRateLimiter(config -> config
+                                                .setRateLimiter(redisRateLimiter())
+                                                .setKeyResolver(hostNameKeyResolver()))
                                         .circuitBreaker(config -> config
-                                        .setName("giftshopBreaker")
-                                        .setFallbackUri("forward:/fallback/users")))
+                                                .setName("giftshopBreaker")
+                                                .setFallbackUri("forward:/fallback/users"))
+                                        .retry(config -> config
+                                                .setRetries(10)
+                                                .setMethods(HttpMethod.GET)))
                                 .uri("lb://USER-SERVICE")) //only possible with eureka.
                                 //.uri("http://localhost:8081"))
                 .route("product-service",
                         r -> r.path("/api/products/**")
                                 .filters(f ->
-                                        f.retry(config -> config
+                                        f.circuitBreaker(config -> config
+                                                .setName("giftshopBreaker")
+                                                .setFallbackUri("forward:/fallback/products"))
+                                        .retry(config -> config
                                                 .setRetries(10)
                                                 .setMethods(HttpMethod.GET)
                                                 .setBackoff(
@@ -40,22 +69,19 @@ public class GatewayConfig {
                                                         Duration.ofSeconds(2),    // max delay
                                                         2,                      // multiplier
                                                         false                     // exponential? (false = fixed)
-                                                ))
-                                        .circuitBreaker(config -> config
-                                        .setName("giftshopBreaker")
-                                        .setFallbackUri("forward:/fallback/products")))
+                                                )))
                                 .uri("lb://PRODUCT-SERVICE"))
                                 //.uri("http://localhost:8082"))
                 .route("order-service",
                         r -> r.path("/api/orders/**", "/api/cart/**")
                                 .filters(f -> f
+                                        .circuitBreaker(config -> config
+                                            .setName("giftshopBreaker")
+                                            .setFallbackUri("forward:/fallback/order"))
                                         .retry(config -> config
                                                 .setRetries(5)
                                                 .setMethods(HttpMethod.GET)
-                                        )
-                                        .circuitBreaker(config -> config
-                                        .setName("giftshopBreaker")
-                                        .setFallbackUri("forward:/fallback/order")))
+                                        ))
                                 .uri("lb://ORDER-SERVICE"))
                                 //.uri("http://localhost:8083"))
                 .route("eureka-server", r -> r
