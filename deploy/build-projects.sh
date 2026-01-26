10e49cc4aebf9eafad0def340ae13099f4422203
houcai <lishannng@hotmail.com>
2026-01-26 18:34:52 +0100

    Add shell script for building all microservices.

---
+++ deploy/build-projects.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ..
+
+# Build all services
+cd order && ./mvnw clean package -DskipTests && cd ..
+cd user && ./mvnw clean package -DskipTests && cd ..
+cd product && ./mvnw clean package -DskipTests && cd ..
+cd notification && ./mvnw clean package -DskipTests && cd ..
+cd eureka && ./mvnw clean package -DskipTests && cd ..
+cd gateway && ./mvnw clean package -DskipTests && cd ..
+cd config-server && ./mvnw clean package -DskipTests && cd ..
